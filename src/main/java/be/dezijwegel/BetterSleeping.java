d60e2f6a9330547a8e28d57eafdb5c45060658f6
Dieter_ntm <nuytemans_dieter@hotmail.com>
2019-09-01 22:43:38 +0200

    Added an update checker

--- src/main/java/be/dezijwegel/BetterSleeping.java
+++ src/main/java/be/dezijwegel/BetterSleeping.java
@@ -13,6 +13,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.LinkedList;
 
 /**
@@ -76,6 +82,56 @@ public class BetterSleeping extends JavaPlugin implements Reloadable {
 
         this.getCommand("bettersleeping").setExecutor(commandHandler);
         this.getCommand("bettersleeping").setTabCompleter(new TabCompletion( onSleepEvent.getSleepTracker() ));
+
+        if (management.getBooleanSetting("update_checker")) {
+            String version = this.getDescription().getVersion();
+            new UpdateChecker( version );
+        }
+    }
+
+    private class UpdateChecker extends Thread {
+
+        private String currentVersion;
+
+        /**
+         * Create an instance of UpdateChecker and perform an update check
+         */
+        public UpdateChecker( String currentVersion)
+        {
+            this.currentVersion = currentVersion;
+            this.run();
+        }
+
+        @Override
+        public void run()
+        {
+            URL url = null;
+            try {
+                url = new URL("https://api.spigotmc.org/legacy/update.php?resource=60837");
+            } catch (MalformedURLException e) {
+                Bukkit.getLogger().info("[BetterSleeping] An error occurred while retrieving the latest version!");
+            }
+
+            URLConnection conn = null;
+            try {
+                conn = url.openConnection();
+            } catch (IOException e) {
+                Bukkit.getLogger().info("[BetterSleeping] An error occurred while retrieving the latest version!");
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String updateVersion = reader.readLine();
+                if (updateVersion.equals(currentVersion)) {
+                    Bukkit.getConsoleSender().sendMessage("[BetterSleeping] You are using the latest version: " + currentVersion + ".");
+                } else {
+                    Bukkit.getConsoleSender().sendMessage("[BetterSleeping] " + ChatColor.RED + "Update detected, you are using version " + currentVersion + " and the latest version is " + updateVersion + "! Download it at https://www.spigotmc.org/resources/bettersleeping-1-12-1-14.60837/");
+                }
+            } catch (IOException e) {
+                Bukkit.getLogger().info("[BetterSleeping] An error occurred while retrieving the latest version!");
+            }
+        }
+
     }
 
 }
