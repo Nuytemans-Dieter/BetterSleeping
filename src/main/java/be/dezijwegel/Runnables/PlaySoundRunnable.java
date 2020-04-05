906eeda3e82d033f13dad84f594f1dc1bcdb5f0c
Dieter Nuytemans <dieter.nuytemans@student.uantwerpen.be>
2020-04-05 20:49:29 +0200

    Implement Easter bells sound on sleep

--- src/main/java/be/dezijwegel/Runnables/PlaySoundRunnable.java
+++ src/main/java/be/dezijwegel/Runnables/PlaySoundRunnable.java
@@ -9,15 +9,27 @@ public class PlaySoundRunnable extends BukkitRunnable {
 
     private Player player;
     private Sound sound;
+    float volume;
+    float pitch;
 
     public PlaySoundRunnable(Player player, Sound sound)
     {
         this.player = player;
         this.sound = sound;
+        volume = 1;
+        pitch = 1;
+    }
+
+    public PlaySoundRunnable(Player player, Sound sound, float volume, float pitch)
+    {
+        this.player = player;
+        this.sound = sound;
+        this.volume = volume;
+        this.pitch = pitch;
     }
 
     @Override
     public void run() {
-        player.playSound(player.getLocation(), sound, 10, 1);
+        player.playSound(player.getLocation(), sound, volume, pitch);
     }
 }
--- src/main/java/be/dezijwegel/timedEvents/easter/EasterEventsHandler.java
+++ src/main/java/be/dezijwegel/timedEvents/easter/EasterEventsHandler.java
@@ -1,15 +1,21 @@
 package be.dezijwegel.timedEvents.easter;
 
 import be.dezijwegel.Runnables.PlaySoundRunnable;
+import be.dezijwegel.Runnables.SendMessageRunnable;
 import be.dezijwegel.management.Management;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
 
 public class EasterEventsHandler implements Listener {
 
@@ -22,6 +28,7 @@ public class EasterEventsHandler implements Listener {
         this.plugin = plugin;
     }
 
+
     @EventHandler
     public void onWakeEvent(PlayerBedLeaveEvent wake)
     {
@@ -30,12 +37,21 @@ public class EasterEventsHandler implements Listener {
 
         Player player = wake.getPlayer();
         long time = player.getWorld().getTime();
-        if (time >= 0 && time <= 5)
+        if (time >= 0 && time <= 20)
         {
             poopEgg(player);
         }
     }
 
+
+    @EventHandler
+    public void onSleepEvent(PlayerBedEnterEvent event)
+    {
+        if (!management.getEventsConfig().getBoolean("easter.enable_jingle"))
+            return;
+
+        playBellsJingle(event.getPlayer());
+    }
 
     /**
      * Let this player poop an egg, hears the egg poop sound and gets a message
@@ -43,11 +59,48 @@ public class EasterEventsHandler implements Listener {
      */
     private void poopEgg(Player player)
     {
+        // Create egg
         ItemStack poopedEgg = new ItemStack(Material.EGG);
 
+        // Change egg name
+        ItemMeta meta = poopedEgg.getItemMeta();
+        String eggName = ChatColor.GOLD + "[BetterSleeping] " + ChatColor.DARK_AQUA + "Easter egg";
+        assert meta != null;
+        meta.setDisplayName(eggName);
+        poopedEgg.setItemMeta(meta);
+
+        // Perform egg pooping actions
         player.getWorld().dropItemNaturally(player.getLocation(), poopedEgg);
         player.playSound(player.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 1);
         management.sendMessage("easter_poopegg", player);
     }
 
+
+    /**
+     * Let the clocks sing for this player
+     * @param player the player to enjoy the sounds
+     */
+    private void playBellsJingle(Player player)
+    {
+        // Pick a sound
+        Sound bell = Sound.BLOCK_NOTE_BLOCK_BELL;
+
+        // Create the jingle
+        PlaySoundRunnable play;
+        play = new PlaySoundRunnable(player, bell, 1, 0.707107f);
+        play.runTaskLater(plugin, 0);
+        play = new PlaySoundRunnable(player, bell, 1, 0.529732f);
+        play.runTaskLater(plugin, 12);
+        play = new PlaySoundRunnable(player, bell, 1, 0.707107f);
+        play.runTaskLater(plugin, 24);
+        play = new PlaySoundRunnable(player, bell, 1, 0.707107f);
+        play.runTaskLater(plugin, 50);
+        play = new PlaySoundRunnable(player, bell, 1, 0.529732f);
+        play.runTaskLater(plugin, 62);
+        play = new PlaySoundRunnable(player, bell, 1, 0.707107f);
+        play.runTaskLater(plugin, 74);
+
+        // Inform the player
+       management.sendMessage("easter_jingle", player);
+    }
 }
