8aa974dc96cfbe749b7709bcfbf8ae52e42d134a
Dieter Nuytemans <Nuytemans_Dieter@hotmail.com>
2021-02-23 22:57:03 +0100

    Fix failing build

--- src/main/java/be/dezijwegel/bettersleeping/util/MetricsHandler.java
+++ src/main/java/be/dezijwegel/bettersleeping/util/MetricsHandler.java
@@ -5,6 +5,8 @@ import be.dezijwegel.bettersleeping.hooks.EssentialsHook;
 import be.dezijwegel.bettersleeping.permissions.BypassChecker;
 import be.dezijwegel.bettersleeping.timechange.TimeChanger;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.AdvancedPie;
+import org.bstats.charts.SimplePie;
 import org.bukkit.GameMode;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,39 +24,39 @@ public class MetricsHandler {
 
         Metrics metrics = new Metrics(plugin, 7414);
 
-        metrics.addCustomChart(new Metrics.SimplePie("spigot_version", () -> plugin.getServer().getBukkitVersion().split("-")[0]));
+        metrics.addCustomChart(new SimplePie("spigot_version", () -> plugin.getServer().getBukkitVersion().split("-")[0]));
 
         String[] substr = localised.split("-");
         String localisation = substr.length >= 2 ? substr[0].toLowerCase() + "-" + substr[1].toUpperCase() : "en-US";
-        metrics.addCustomChart(new Metrics.SimplePie("localization", () -> localisation));
+        metrics.addCustomChart(new SimplePie("localization", () -> localisation));
 
-        metrics.addCustomChart(new Metrics.SimplePie("auto_add_missing_options", () -> String.valueOf(autoAddMissingOptions)));
+        metrics.addCustomChart(new SimplePie("auto_add_missing_options", () -> String.valueOf(autoAddMissingOptions)));
 
-        metrics.addCustomChart(new Metrics.SimplePie("server_has_essentials", () -> essentialsHook.isHooked() ? "Yes" : "No"));
+        metrics.addCustomChart(new SimplePie("server_has_essentials", () -> essentialsHook.isHooked() ? "Yes" : "No"));
 
-        metrics.addCustomChart(new Metrics.SimplePie("server_has_placeholderapi", () -> plugin.getServer().getPluginManager().getPlugin("PlaceholderAPI")!=null ? "Yes" : "No"));
+        metrics.addCustomChart(new SimplePie("server_has_placeholderapi", () -> plugin.getServer().getPluginManager().getPlugin("PlaceholderAPI")!=null ? "Yes" : "No"));
 
         if (sleeperCalculatorType != null)
         {
-            metrics.addCustomChart(new Metrics.SimplePie("sleepers_calculator", sleeperCalculatorType::toLowerCase));
+            metrics.addCustomChart(new SimplePie("sleepers_calculator", sleeperCalculatorType::toLowerCase));
 
             if (sleeperCalculatorType.equalsIgnoreCase("percentage"))
             {
-                metrics.addCustomChart(new Metrics.SimplePie("percentage_needed", () -> String.valueOf(percentageNeeded)));
+                metrics.addCustomChart(new SimplePie("percentage_needed", () -> String.valueOf(percentageNeeded)));
             }
             else if (sleeperCalculatorType.equalsIgnoreCase("absolute"))
             {
-                metrics.addCustomChart(new Metrics.SimplePie("absolute_needed", () -> String.valueOf(absoluteNeeded)));
+                metrics.addCustomChart(new SimplePie("absolute_needed", () -> String.valueOf(absoluteNeeded)));
             }
         }
 
-        metrics.addCustomChart(new Metrics.SimplePie("time_changer_type", () -> timeChangerType.name().toLowerCase()));
+        metrics.addCustomChart(new SimplePie("time_changer_type", () -> timeChangerType.name().toLowerCase()));
 
-        metrics.addCustomChart(new Metrics.SimplePie("enable_bypass", () -> bypassChecker.isEnabled() ? "Yes" : "No"));
+        metrics.addCustomChart(new SimplePie("enable_bypass", () -> bypassChecker.isEnabled() ? "Yes" : "No"));
 
-        metrics.addCustomChart(new Metrics.SimplePie("shortens_prefix", () -> shortenPrefix ? "Yes" : "No"));
+        metrics.addCustomChart(new SimplePie("shortens_prefix", () -> shortenPrefix ? "Yes" : "No"));
 
-        metrics.addCustomChart(new Metrics.AdvancedPie("bypassed_gamemodes", () -> {
+        metrics.addCustomChart(new AdvancedPie("bypassed_gamemodes", () -> {
             Map<String, Integer> valueMap = new HashMap<>();
 
             // Add all bypassed gamemodes
@@ -70,13 +72,13 @@ public class MetricsHandler {
             return valueMap;
         }));
 
-        metrics.addCustomChart(new Metrics.SimplePie("uses_buffs", () -> buffsHandler.getBuffs().size() != 0 ? "Yes" : "No"));
+        metrics.addCustomChart(new SimplePie("uses_buffs", () -> buffsHandler.getBuffs().size() != 0 ? "Yes" : "No"));
 
-        metrics.addCustomChart(new Metrics.SimplePie("uses_debuffs", () -> buffsHandler.getDebuffs().size() != 0 ? "Yes" : "No" ));
+        metrics.addCustomChart(new SimplePie("uses_debuffs", () -> buffsHandler.getDebuffs().size() != 0 ? "Yes" : "No" ));
 
-        metrics.addCustomChart(new Metrics.SimplePie("is_multiworld", () -> isMultiworld ? "Yes" : "No" ));
+        metrics.addCustomChart(new SimplePie("is_multiworld", () -> isMultiworld ? "Yes" : "No" ));
 
-        metrics.addCustomChart(new Metrics.SimplePie("is_premium", () -> "No" ));
+        metrics.addCustomChart(new SimplePie("is_premium", () -> "No" ));
 
     }
 
