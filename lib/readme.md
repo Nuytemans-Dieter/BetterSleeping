cbc5d11d21767e1dfb9396bc70f8214e9176e63f
Dieter Nuytemans <dieter.nuytemans@student.uantwerpen.be>
2021-07-02 17:36:26 +0200

    Add GSIT support

--- lib/readme.md
+++ lib/readme.md
@@ -6,7 +6,7 @@ Libraries in this folder are solely used as dependencies to ensure compatibility
 For each file, an official download location can be found in the table below.
 
 If you are the owner of any library in this folder and want your artifact removed, give me a notice through an issue and I will remove them at once.
-Keep in mind that I may drop support for your plugin until it can be included as a Maven dependency to keep cloning the project simple.
+Keep in mind that support for your plugin may be dropped, until it can be included as a Maven dependency to keep cloning the project simple.
 
 | Library | Download location |
 | ------- | ----------------- |
--- src/main/java/be/betterplugins/bettersleeping/hooks/events/GSitListener.java
+++ src/main/java/be/betterplugins/bettersleeping/hooks/events/GSitListener.java
@@ -1,51 +1,62 @@
-//package be.dezijwegel.bettersleeping.hooks.events;
-//
-//import be.dezijwegel.bettersleeping.runnables.SleepersRunnable;
-//import be.dezijwegel.bettersleeping.util.SleepTimeChecker;
-//import me.gsit.api.events.PlayerGetUpLayEvent;
-//import me.gsit.api.events.PlayerLayEvent;
-//import org.bukkit.World;
-//import org.bukkit.entity.Player;
-//import org.bukkit.event.EventHandler;
-//import org.bukkit.event.EventPriority;
-//import org.bukkit.event.Listener;
-//
-//import java.util.Map;
-//
-//public class GSitListener implements Listener {
-//
-//    private final Map<World, SleepersRunnable> sleepHandlers;
-//
-//    public GSitListener( Map<World, SleepersRunnable> sleepHandlers )
-//    {
-//        this.sleepHandlers = sleepHandlers;
-//    }
-//
-//    @EventHandler(priority=EventPriority.HIGHEST)
-//    public void onPlayerLay (PlayerLayEvent playerLayEvent)
-//    {
-//        Player player = playerLayEvent.getPlayer();
-//
-//        // If time for sleeping has not come yet, don't count the event
-//        if (! SleepTimeChecker.isSleepPossible( player.getWorld() ))
-//            return;
-//
-//        // Don't handle disabled worlds
-//        if ( ! sleepHandlers.containsKey( player.getWorld() ))
-//            return;
-//
-//        sleepHandlers.get( player.getWorld() ).playerCustomEnterBed( player );
-//    }
-//
-//    @EventHandler(priority=EventPriority.HIGHEST)
-//    public void onPlayerGetUp(PlayerGetUpLayEvent playerStandUpLayEvent)
-//    {
-//        Player player = playerStandUpLayEvent.getPlayer();
-//
-//        // Don't handle disabled worlds
-//        if ( ! sleepHandlers.containsKey( player.getWorld() ))
-//            return;
-//
-//        sleepHandlers.get( player.getWorld() ).playerCustomLeaveBed( player );
-//    }
-//}
+package be.betterplugins.bettersleeping.hooks.events;
+
+import be.betterplugins.bettersleeping.model.SleepWorldManager;
+import be.betterplugins.bettersleeping.util.TimeUtil;
+import me.gsit.api.events.PlayerGetUpLayEvent;
+import me.gsit.api.events.PlayerLayEvent;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Pose;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import java.util.Map;
+
+public class GSitListener implements Listener {
+
+    private final SleepWorldManager sleepWorldManager;
+
+    public GSitListener( SleepWorldManager sleepWorldManager )
+    {
+        this.sleepWorldManager = sleepWorldManager;
+    }
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onPlayerLay (PlayerLayEvent playerLayEvent)
+    {
+        Player player = playerLayEvent.getPlayer();
+        World world = player.getWorld();
+
+        // If time for sleeping has not come yet, don't count the event
+        if (! TimeUtil.isSleepPossible( world ))
+        {
+            return;
+        }
+
+        // Don't handle disabled worlds
+        if ( !sleepWorldManager.isWorldEnabled( world ) )
+        {
+            return;
+        }
+
+        if (playerLayEvent.getPose() == Pose.SLEEPING)
+        {
+            sleepWorldManager.addSleeper(player);
+        }
+    }
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onPlayerGetUp(PlayerGetUpLayEvent playerStandUpLayEvent)
+    {
+        Player player = playerStandUpLayEvent.getPlayer();
+
+        // Don't handle disabled worlds
+        if ( ! sleepWorldManager.isWorldEnabled( player.getWorld() ))
+        {
+            return;
+        }
+
+        this.sleepWorldManager.removeSleeper( player );
+    }
+}
