2b6eadc445b13cef7f15463d2a5e5d8936c0bcf6
Dieter Nuytemans <dieter.nuytemans@student.uantwerpen.be>
2021-07-19 12:50:58 +0200

    Fix num sleepers placeholder

--- src/main/java/be/betterplugins/bettersleeping/runnables/SleepRunnable.java
+++ src/main/java/be/betterplugins/bettersleeping/runnables/SleepRunnable.java
@@ -82,13 +82,14 @@ public class SleepRunnable extends BukkitRunnable
 
         SleepStatus sleepStatus = getSleepStatus();
 
+        if (!sleeper.isSleeping())
+            sleepers.add( sleeper.getUniqueId() );
+
         this.messenger.sendMessage(players,"bed_enter_broadcast",
                 new MsgEntry("<num_sleeping>", sleepStatus.getNumSleepers()),
                 new MsgEntry("<needed_sleeping>", sleepStatus.getNumNeeded()),
                 new MsgEntry("<remaining_sleeping>", sleepStatus.getNumMissing()),
                 new MsgEntry("<player>", sleeper.getName()));
-        if (!sleeper.isSleeping())
-            sleepers.add( sleeper.getUniqueId() );
     }
 
 
